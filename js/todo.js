f648ca2a0e79d3a8a73505bb9a01096939c3c035
linkcoding <kenny.lin.91@gmail.com>
2017-09-08 13:09:37 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm()
 });
 
-// define functions here
+function submitForm(){
+  $('form').on('submit', (e) =>{
+    e.preventDefault()
+    let input = $('#item').val()
+    // console.log(input);
+    $('#list ol')[0].innerHTML += `<li>${input}</li>`
+    // console.log($('#list ol'));
+  })
+}
